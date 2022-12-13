ab2e91fd8d0eb8d5d8d906ec2ab7d97aa9a114c3
theanuragharsh <nrgharsh@gmail.com>
2022-12-13 12:15:56 +0530

    Initial commit

---
+++ src/main/java/com/threadingbasics/InterThreadCommunicationsApplication.java
@@ -0,0 +1,13 @@
+package com.threadingbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterThreadCommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterThreadCommunicationsApplication.class, args);
+	}
+
+}
